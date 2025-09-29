9b432e7e2c4b7beb04988d6183899e572b51d166
anastasiyanasevich <nastianosevich@gmail.com>
2025-09-29 19:59:26 +0400

    [feature] calculated maxNumAfterComma

--- test.js
+++ test.js
@@ -1,16 +1,26 @@
 const sumInputsEqualZero = (nums) => {
     let sum = 0;
+    let maxNumAfterComma = 0;
 
     for (const num of nums) {
         if (typeof num !== 'number' || Number.isNaN(num)) {
             throw new Error('provide number');
         }
 
-        sum += Math.round(num * 100); // keep in mind that ita money and usually we have 2 digits after comma
+        const parts = num.toString().split('.');
+
+
+        if (parts[1]) {
+            maxNumAfterComma = Math.max(maxNumAfterComma, parts[1].length);
+        }
     }
 
-    // const sumWithTwoDigitsAfterComma = sum.toFixed(2);  // lol, made a tiny hack :)
-    // return +sumWithTwoDigitsAfterComma === 0;
+    const x = 10 ** maxNumAfterComma;
+    // console.log(x);
+
+    for (const num of nums) {
+        sum += Math.round(num * x);
+    }
 
     return sum === 0;
 };
@@ -19,8 +29,12 @@ const nums = [1, 2, 3]; // false
 const nums2 = [1, 2, 'dfdf']; // error
 const nums3 = [1, 2, -3]; // true
 const nums4 = [0.1, 0.2, -0.3]; // true
+const nums5 = [1.001, 2.002, -3.003]; // true
+const nums6 = [1.0000001, 2.0000002, -3.0000003]; // true
 
 console.log('result', sumInputsEqualZero(nums));
 // console.log('result2', sumInputsEqualZero(nums2));
 console.log('result3', sumInputsEqualZero(nums3));
 console.log('result4', sumInputsEqualZero(nums4));
+console.log('result5', sumInputsEqualZero(nums5));
+console.log('result6', sumInputsEqualZero(nums6));
